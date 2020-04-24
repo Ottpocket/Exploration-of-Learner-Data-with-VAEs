74ad8768d316823417987eb538beaf8a038dc114
Ottpocket <64142335+Ottpocket@users.noreply.github.com>
2020-04-24 10:20:52 -0500

    Created Replication Main

---
+++ Replication_main.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Replication __main__
+This function creates a NN as described in Autoencoders for Education 
+Assessment, and creates the graphs just like that paper.  Specifically, we 
+replicate table 1, figure 3, figure 4, table 2, and figure 5.  
+@author: andre
+"""
+
