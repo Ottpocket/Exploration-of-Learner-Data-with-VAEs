d3a4b7e1c632982704e9a397a58bf2585bf57e9a
Ottpocket <64142335+Ottpocket@users.noreply.github.com>
2020-05-11 15:05:53 -0500

    Update Graphical_Functions.py
    Changed to help prevent nan inf messing crap up

--- Graphical_Functions.py
+++ Graphical_Functions.py
@@ -64,9 +64,9 @@ def Get_stats(H, qmat, amat, bvec, students, thetas, tests, questions, network_n
               studtest, dist, arch_type, activation, dropout_rate):
     #H = history_dict
     #studtest = data.values[:,0:2]
-    A_list = [np.exp(a) for a in H.history['log_A']]
-    B_list = H.history['B']
-    th = H.history['thetas']
+    A_list = [np.exp(a) for a in H.history['log_A']][0:-3] #the last subscript to get best val in earlystopped training
+    B_list = H.history['B'][0:-3]
+    th = H.history['thetas'][0:-3]
     dfa, dfb = get_stats_over_time(A_list, B_list, amat, bvec, qmat, matrix= True)
     th_avrb, th_RMSE, th_Corr = get_theta_stats_v2(thetas, th[-1], studtest)
     df_row = [students, tests, questions, dist, arch_type, activation, dropout_rate, network_num, 
