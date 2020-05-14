42d63fbc598b5f5f58cd5001e519d9a562a67753
Ottpocket <64142335+Ottpocket@users.noreply.github.com>
2020-05-14 06:57:15 -0500

    Update main_testing.py

--- main_testing.py
+++ main_testing.py
@@ -6,6 +6,10 @@ Main:  Playground for testing implemented functions
 import pandas as pd
 import os
 os.chdir("C:/Users/andre/Documents/GitHub/Exploration-of-Learner-Data-with-VAEs")
+import seaborn as sns
+import matplotlib.pyplot as plt
+sns.set_color_codes("colorblind")
+from Replication_main import Replication_of_Paper_Figures
 from Teaching_Vae_Class import Teaching_Vae
 from Data_Gen import Create_data
 from Experiment_table_Function import Experiment_table
@@ -42,11 +46,6 @@ g.savefig('JibberJabber.png')
 ###############################################################################
 #Plot of fig3
 ###############################################################################
-import seaborn as sns
-import matplotlib.pyplot as plt
-sns.set_color_codes("colorblind")
-from scipy.stats import pearsonr
-from Replication_main import Replication_of_Paper_Figures
 tab1, fig3, fig4, tab2, fig5, vae, ae, data_list = Replication_of_Paper_Figures()
 
 fig, ax =plt.subplots(nrows = 1, ncols = 2, figsize = (15, 5))
@@ -59,11 +58,38 @@ ax[1].set_ylim([0.0,4])
 fig.show()
 fig.savefig('Parameter Recovery.png')
 
-pearsonr(fig3[fig3['A_i']==2].True_Values, fig3[fig3['A_i']==2].Estimates_vae)[0]
+###############################################################################
+#Plot of fig 4
+###############################################################################
+fig, ax =plt.subplots(nrows = 1, ncols = 2, figsize = (15, 5))
+sns.scatterplot(x = 'True_Values',y= 'Estimates_ae',  ax=ax[0],  
+                data = fig4).set_title('AE Parameter Recovery')
+sns.scatterplot(x = 'True_Values',y= 'Estimates_vae',  ax=ax[1],  
+                data = fig4).set_title('VAE Parameter Recovery')
+ax[0].set_ylim([-4.0,4.0])
+ax[1].set_ylim([-4.0,4.0])
+fig.show()
+fig.savefig('fig4.png')
 
+###############################################################################
+#Making Table 2 pretty
+###############################################################################
+Table2 = pd.DataFrame({'Statistic': ['AVRB', 'AVRB', 'CORR','CORR', 'RMSE','RMSE'],
+                       'Model': ['VAE','AE','VAE','AE','VAE','AE'],
+                       'Theta1': tab2['Theta1'],
+                       'Theta2': tab2['Theta2'],
+                       'Theta3': tab2['Theta3'],})
 
-fig3.groupby('A_i').agg({'Estimates_ae':'count'})
-fig, ax = plt.subplots(3, 2, sharex='col', sharey='row')
-
-
+###############################################################################
+#Plot of fig 5
+###############################################################################
+fig, ax =plt.subplots(nrows = 1, ncols = 2, figsize = (15, 5))
+sns.scatterplot(x = 'Theta1_true',y= 'Theta1_ae',  ax=ax[0],  
+                data = fig5).set_title('AE prediction of 1st latent trait')
+sns.scatterplot(x = 'Theta1_true',y= 'Theta1_vae',  ax=ax[1],  
+                data = fig5).set_title('VAE prediction of 1st latent trait')
+ax[0].set_ylim([-4.0,4.0])
+ax[1].set_ylim([-4.0,4.0])
+fig.show()
+fig.savefig('fig5.png')
 
